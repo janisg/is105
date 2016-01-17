295c85caac3f841fcc2a39d5614bea9bcc6454d6
Janis Gailis <tellme@jaanis.org>
2016-01-17 21:39:35 +0100

    The unit test for the add function from the is105 module.

---
+++ python/test_add_is105.py
@@ -0,0 +1,13 @@
+import unittest
+from is105 import add
+
+class TestAdd(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_numbers_5_7(self):
+        self.assertEqual(add(5,7), 11)
+    def test_string_a_b(self):
+        self.assertEqual(add('a','b'), 'ab')
+        
+if __name__ == '__main__':
+    unittest.main()
